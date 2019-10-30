3793145e68515878f9e5580e82772a627f7a5f9d
kmartin62 <martinkostadinov997@gmail.com>
2019-10-30 16:34:01 +0100

    slight changes

--- src/containers/TestComponent/TestComponent.js
+++ src/containers/TestComponent/TestComponent.js
@@ -2,21 +2,20 @@ import ApexCharts from 'apexcharts';
 import React, { Component } from "react";
 import ReactApexCharts from 'react-apexcharts'
 
-let lastDate = 0;
-let data = [];
-let TICKINTERVAL = 86400000;
-let XAXISRANGE = 777600000;
-
+var lastDate = 0;
+var data = []
+var TICKINTERVAL = 86400000
+let XAXISRANGE = 777600000
 function getDayWiseTimeSeries(baseval, count, yrange) {
-    let i = 0;
+    var i = 0;
     while (i < count) {
-        let x = baseval;
-        let y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+        var x = baseval;
+        var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
 
         data.push({
             x, y
         });
-        lastDate = baseval;
+        lastDate = baseval
         baseval += TICKINTERVAL;
         i++;
     }
@@ -25,17 +24,17 @@ function getDayWiseTimeSeries(baseval, count, yrange) {
 getDayWiseTimeSeries(new Date('11 Feb 2017 GMT').getTime(), 10, {
     min: 10,
     max: 90
-});
+})
 
 function getNewSeries(baseval, yrange) {
-    let newDate = baseval + TICKINTERVAL;
-    lastDate = newDate;
+    var newDate = baseval + TICKINTERVAL;
+    lastDate = newDate
 
-    for(let i = 0; i< data.length - 10; i++) {
+    for(var i = 0; i< data.length - 10; i++) {
         // IMPORTANT
         // we reset the x and y of the data which is out of drawing area
         // to prevent memory leaks
-        data[i].x = 0;
+        data[i].x = newDate - XAXISRANGE - TICKINTERVAL
         data[i].y = 0
     }
 
@@ -81,23 +80,22 @@ class TestComponent extends React.Component {
                   enabled: false
                 },
                 stroke: {
+                    show: true,
                     curve: 'smooth',
-                    width: 8
-                },
-
-                title: {
-                    // text: 'Dynamic Updating Chart',
-                    align: 'left'
+                    lineCap: 'butt',
+                    colors: undefined,
+                    width: 8,
+                    dashArray: 0,
                 },
                 markers: {
                     size: 0
                 },
                 xaxis: {
                     type: 'datetime',
+                    range: XAXISRANGE,
                     labels: {
                         show: false
                     },
-                    range: 0,
                 },
                 yaxis: {
                     max: 100
@@ -121,7 +119,7 @@ class TestComponent extends React.Component {
             getNewSeries(lastDate, {
                 min: 10,
                 max: 90
-            });
+            })
 
             ApexCharts.exec('realtime', 'updateSeries', [{
                 data: data
